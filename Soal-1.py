ec2f011dbe940bfb12615fc46c35992b11677c4d
waan69 <82877721+waan69@users.noreply.github.com>
2021-04-24 07:20:21 +0700

    Add files via upload

---
+++ Soal-1.py
@@ -0,0 +1,5 @@
+name = input("Masukan nama: ")
+age = int(input("Masukan umur: "))
+height = float(input("Masukan tinggi badan (cm): "))
+print("Perkenalkan nama saya" ,name, "umur saya" ,age, "tahun dan tinggi saya " ,height,)
+
